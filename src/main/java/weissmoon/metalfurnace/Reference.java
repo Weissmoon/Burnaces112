b93bfbcd5ab3875eb0cf2b67097206269e1943a3
weissmoon <weissmoon1@gmail.com>
2019-11-15 22:18:55 +0900

    Update version

--- src/main/java/weissmoon/metalfurnace/Reference.java
+++ src/main/java/weissmoon/metalfurnace/Reference.java
@@ -6,7 +6,7 @@ package weissmoon.metalfurnace;
 public class Reference {
     public static final String MOD_ID = "metalfurnace";
     public static final String MOD_NAME = "Metal Furnace";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 //    public static final String GUI_FACTORY_CLASS = "weissmoon.random.client.gui.GuiFactory";
 //    public static final String CLIENT_PROXY = "weissmoon.random.proxy.ClientProxy";
 //    public static final String SERVER_PROXY = "weissmoon.random.proxy.ServerProxy";
